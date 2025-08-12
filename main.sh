e5af1a79e421b142d25ddff2b43d760235d3f692
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-08-12 17:24:03 -0300

    Update repository URLs for Debian 11

--- main.sh
+++ main.sh
@@ -229,7 +229,7 @@ while read item; do
 	fi
 	
 	if (( debian && distribution_version <= 7 )) || (( ubuntu && distribution_version <= 12 )); then
-		patch --directory="${sysroot_directory}/include" --strip='1' --input="${workdir}/patches/0001-Alias-static_assert-to-_Static_assert-in-C11-mode.patch"
+		patch --reject-file='/tmp/null' --no-backup-if-mismatch --directory="${sysroot_directory}/include" --strip='1' --input="${workdir}/patches/0001-Alias-static_assert-to-_Static_assert-in-C11-mode.patch" || true
 	fi
 	
 	cd "${temporary_directory}"
