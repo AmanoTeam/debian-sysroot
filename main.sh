e868d53dfd3f9fa75239e6785cecd27c5758bc0f
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-04-15 17:27:49 -0300

    Fix missing libnss_db on glibc 2.15

--- main.sh
+++ main.sh
@@ -272,10 +272,6 @@ while read item; do
 			--input="${workdir}/patches/ptrace/x86_64-unknown-linux-gnu${glibc_version}/0001-Backport-PTRACE_-definitions-from-glibc-2.27-to-older-glibc-releases.patch" 2>/dev/null || true
 	fi
 	
-	if [ "${triplet}" = 'i386-unknown-linux-gnu' ] && (( ubuntu && distribution_version == 12 )); then
-		rm "${sysroot_directory}/lib/libnss_db.so"*
-	fi
-	
 	if (( debian && distribution_version == 5 )); then
 		patch \
 			--reject-file='/tmp/null' \
